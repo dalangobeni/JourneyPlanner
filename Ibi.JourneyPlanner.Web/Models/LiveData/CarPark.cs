eb13b2dc869986c2740dd4d2d6c5b9832b4eaf14
Kevin Wilson <kevin@kwilson.me.uk>
2013-03-24 01:21:49 +0000

    Added basics for live data API service.

---
+++ Ibi.JourneyPlanner.Web/Models/LiveData/CarPark.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ibi.JourneyPlanner.Web.Models.LiveData
+{
+    public class CarPark
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string State { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string SCN { get; set; }
+        public int Capacity { get; set; }
+        public int SpacesNow { get; set; }
+        public int PredictedSpaces60Mins { get; set; }
+        public int PredictedSpaces30Mins { get; set; }
+    }
+}
